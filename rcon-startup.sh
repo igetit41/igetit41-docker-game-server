d1bd8680356ad4fb25df01420a43b43153143d52
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-03-22 22:46:51 -0700

    rcon

--- rcon-startup.sh
+++ rcon-startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Project Zomboid
 
-sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW setaccesslevel D3F1L3 admin
+RCON_STARTUP=$(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW setaccesslevel D3F1L3 admin)
+echo $RCON_STARTUP
+
+
