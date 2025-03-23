33808e38e16ff9dc2f38cbe6c2c5bb5a1b7c6e7d
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-03-22 19:15:19 -0700

    rcon pw

--- rcon-startup.sh
+++ rcon-startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Project Zomboid
+echo "-----startup-script-output-rcon-startup"
+
+export RCON_PW=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/RCON_PW" -H "Metadata-Flavor: Google")
+echo $RCON_PW
+
 sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW setaccesslevel D3F1L3 admin
