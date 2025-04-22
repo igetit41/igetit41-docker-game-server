616ff442927dbe76767183e06701ecd3b0c340ce
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-04-21 21:44:18 -0700

    delete unneeded files

--- player-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Project Zomboid
-
-PLAYERS=$(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW players | grep -Eo '[0-9]+' | head -1)
-echo $PLAYERS
--- rcon-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Project Zomboid
-
-RCON_STARTUP=$(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW setaccesslevel D3F1L3 admin)
-echo $RCON_STARTUP
-
-
