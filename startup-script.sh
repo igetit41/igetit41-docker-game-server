539aa439daeb05da4adec205d5001ac27e3cc152
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-04-22 23:03:38 -0700

    split and output

--- startup-script.sh
+++ startup-script.sh
@@ -211,8 +211,9 @@ done
 while true; do
     echo "-----startup-script-output-player-check"
     #PLAYERS=$(sudo /home/game-server/igetit41-docker-game-server/player-check.sh)
-    
-    PLAYERS=$(echo $(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW "$RCON_PLAYER_CHECK") | $RCON_PLAYER_CHECK_GREP)
+    RCON_OUTPUT=$(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW "$RCON_PLAYER_CHECK")
+    echo "-----startup-script-output-RCON_OUTPUT: $RCON_OUTPUT"
+    PLAYERS=$(echo "$RCON_OUTPUT" | $RCON_PLAYER_CHECK_GREP)
     #PLAYERS=$(sudo docker exec -i game-server ./rcon-0.10.3-amd64_linux/rcon -a 127.0.0.1:27015 -p $RCON_PW $RCON_PLAYER_CHECK)
     STAMP=$(date +'%Y-%m-%d:%H.%M:%S')
     echo "-----startup-script-output-$STAMP-PLAYERS: $PLAYERS"
