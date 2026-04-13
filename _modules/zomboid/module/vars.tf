71324fc2cb4dc3af0ba9b2d762dbf78576fa4976
igetit41 <45726571+igetit41@users.noreply.github.com>
2026-04-13 11:49:46 -0700

    syntax

--- _modules/zomboid/module/vars.tf
+++ _modules/zomboid/module/vars.tf
@@ -74,7 +74,7 @@ output "exec_commands" {
     value = join(";", [
         "sed -i '/CharacterFreePoints/s/.*/    CharacterFreePoints = 12,/' ./Zomboid/Server/channel27_SandboxVars.lua",
         "sed -i '/StarterKit/s/.*/    StarterKit = true,/' ./Zomboid/Server/channel27_SandboxVars.lua",
-        "sed -i '/StarterKit/s/.*/    StarterKit = true,\n    MaximumDismantlingXpLevel = 10,\n    XpMultiplier = 4,/' ./Zomboid/Server/channel27_SandboxVars.lua",
+        "sed -i '/StarterKit/s/.*/    StarterKit = true,\\n    MaximumDismantlingXpLevel = 10,\\n    XpMultiplier = 4,/' ./Zomboid/Server/channel27_SandboxVars.lua",
         "sed -i '/MinutesPerPage/s/.*/    MinutesPerPage = 0.1,/' ./Zomboid/Server/channel27_SandboxVars.lua",
         "sed -i '/WaterShutModifier/s/.*/    WaterShutModifier = 120,/' ./Zomboid/Server/channel27_SandboxVars.lua",
         "sed -i '/ElecShutModifier/s/.*/    ElecShutModifier = 60,/' ./Zomboid/Server/channel27_SandboxVars.lua",
